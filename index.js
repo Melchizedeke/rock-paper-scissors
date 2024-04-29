9f7fb8cae9edf858fd258154c2189e0a33b14874
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2024-04-29 19:38:13 +0100

    Added comments to the Javascript code

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+//Random number between 0-2 to select an item from the array
 let randomNumber1 = Math.floor(Math.random() * 3)
 let randomNumber2 = Math.floor(Math.random() * 3)
 
+//The game array
 let gameChoices = ['Rock', 'Paper', 'Scissors']
+
+//Random selection of choices
 let playerOne = gameChoices[randomNumber1]
 let computerPlayer = gameChoices[randomNumber2]
 
+//Game logic
 if (playerOne === computerPlayer) {
     document.querySelector('.tag').innerHTML = 'Draw, Play Again!';
 } else if (playerOne === 'Rock' && computerPlayer === 'Scissors') {
@@ -17,11 +22,12 @@ if (playerOne === computerPlayer) {
     document.querySelector('.tag').innerHTML = 'Computer Wins!'
 }
 
+//Selecting the button via the DOM
 let button = document.querySelector('button');
 
+//Assigning values to the main elements for choices
 let pOne = document.querySelector('#p-one')
 pOne.innerHTML = playerOne
-
 let cptr = document.querySelector('#cpu')
 cptr.innerHTML = computerPlayer
 
